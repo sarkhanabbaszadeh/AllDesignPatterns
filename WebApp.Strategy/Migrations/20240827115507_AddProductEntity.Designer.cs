78a63ccd8107125473e4e27f23d43ed67d30eaf7
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2024-08-27 17:03:43 +0400

    Created Products table and set Mongodb connection string

--- WebApp.Strategy/Migrations/20240827115507_AddProductEntity.Designer.cs
+++ WebApp.Strategy/Migrations/20240827115507_AddProductEntity.Designer.cs
@@ -4,6 +4,7 @@ using BaseProject.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BaseProject.Migrations
 {
     [DbContext(typeof(AppIdentityDBContext))]
-    partial class AppIdentityDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240827115507_AddProductEntity")]
+    partial class AddProductEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
