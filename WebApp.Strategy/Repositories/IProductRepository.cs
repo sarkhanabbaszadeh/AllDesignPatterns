f80816535bacc9a92cb861f0fff3279dfd06d249
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2024-08-28 10:49:23 +0400

    Added iproductrepository interface

---
+++ WebApp.Strategy/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetbyId(string id);
+
+        Task<List<Product>> GetAllByUserId(string userId);
+
+        Task Update(Product product);
+
+        Task Delete(Product product);
+    }
+}
